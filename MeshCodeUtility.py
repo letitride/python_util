378f819907910cc8f3269676b83d9ac0ab1e0be2
letitride <ichikawa1@hotmail.co.jp>
2011-12-15 10:08:47 +0900

    float型キャスト時のバグを修正

--- MeshCodeUtility.py
+++ MeshCodeUtility.py
@@ -11,7 +11,7 @@ class MeshCodeUtility(object):
 		left_operator  = int( math.floor(lat * 15 / 10 ) )
 		right_operator = int( math.floor(lon - 100 ) )
 		#南西端のlat,lon
-		dest_lat = left_operator / 15 * 10.0
+		dest_lat = left_operator / 15.0 * 10
 		dest_lon = right_operator + 100.0
 
 		src = {"mesh_code":str(left_operator)+str(right_operator), "lat":dest_lat, "lon":dest_lon}
@@ -24,11 +24,11 @@ class MeshCodeUtility(object):
 
 		#2次メッシュは緯度方向5分(5/60=0.08333)区切り
 		left_operator = int(math.floor((lat - base_data["lat"]) * 100000 / 8333))
-        #経度方向7分30秒(7/60+30/60/60=0.11666+0.008333=0.1249))区切り
+		#経度方向7分30秒(7/60+30/60/60=0.11666+0.008333=0.1249))区切り
 		right_operator = int(math.floor((lon - base_data["lon"]) * 1000 / 125))
 		#南西端のlat,lon
-		dest_lat = left_operator * 8333 / 100000.0 + base_data["lat"]
-		dest_lon = right_operator * 125 / 1000.0   + base_data["lon"]
+		dest_lat = left_operator * 8333.0 / 100000 + base_data["lat"]
+		dest_lon = right_operator * 125.0 / 1000   + base_data["lon"]
 		src = {"mesh_code":base_data["mesh_code"]+str(left_operator)+str(right_operator), "lat":dest_lat, "lon":dest_lon}
 		return src
 
@@ -42,8 +42,8 @@ class MeshCodeUtility(object):
 		#経度方向 45秒(45/60/60=0.0125)区切り
 		right_operator = int(math.floor((lon - base_data["lon"]) * 10000 / 125))
 		#南西端のlat,lon
-		dest_lat = left_operator * 8333 / 1000000.0 + base_data["lat"]
-		dest_lon = right_operator * 125 / 10000.0 + base_data["lon"]
+		dest_lat = left_operator * 8333.0 / 1000000 + base_data["lat"]
+		dest_lon = right_operator * 125.0 / 10000 + base_data["lon"]
 
 		src = {"mesh_code":base_data["mesh_code"]+str(left_operator)+str(right_operator), "lat":dest_lat, "lon":dest_lon}
 		return src
